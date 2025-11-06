8fbec90d4cb225c369435ec2c8b373aa7e3841f9
Sang Bui <sangbui51@yahoo.com>
2025-11-05 17:30:09 -0800

    Created notes of different patterns. Starting with stacks, queues, and sliding windows (fixed and dynamic). Worked on some more problems with these patterns.

--- sliding_window_dynamic.py
+++ sliding_window_dynamic.py
@@ -1,27 +1,59 @@
+from typing import List
 """
 A generic template for dynamic sliding window finding min window length
 """
 def shortest_window(nums, condition):
-    i = 0
+    left = 0
     min_length = float('inf')
     result = None
 
-    for j in range(len(nums)):
+    for right in range(len(nums)):
         # Expand the window
         # Add nums[j] to the current window logic
 
         # Shrink window as long as the condition is met
         while condition():  
             # Update the result if the current window is smaller
-            if j - i + 1 < min_length:
-                min_length = j - i + 1
+            if right - left + 1 < min_length:
+                min_length = right - left + 1
                 # Add business logic to update result
 
             # Shrink the window from the left
             # Remove nums[i] from the current window logic
-            i += 1
+            left += 1
 
     return result
+
+def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        """
+            Find the minimum subarray with the given target sum.
+            **Note**: Use dynamic window to solve this problem
+            Consist of expanding the window and shrinking the window
+        """
+        import math
+
+        if not target or not nums:
+            return 0
+        left = 0
+        window_sum = 0
+
+        min_length = math.inf 
+
+        for right in range(len(nums)):
+            # expand the window until we get to the target sum
+            # usually on the right side
+            window_sum += nums[right]
+
+            # shrink the window to the smallest length
+            # (usually on the left side)
+            while window_sum >= target:
+                current_length = right - left + 1
+                min_length = min(min_length, current_length)
+
+                window_sum -= nums[left]
+                left += 1
+        
+        return 0 if min_length == math.inf else min_length
 
 
 """
@@ -51,7 +83,10 @@ def longest_window(nums, condition):
 
 
 def length_of_longest_substring(s: str) -> int:
-
+    """
+        Find the length of the longest substring without repeating characters
+    """
+    # use a set to check for duplicate
     window_chars = set()
 
     left = 0
@@ -59,17 +94,14 @@ def length_of_longest_substring(s: str) -> int:
 
     for right in range(len(s)):
 
+        # shrink until there is no duplicate
         while s[right] in window_chars:
             window_chars.remove(s[left])
             left += 1
-
+        
+        # expand automatically to try to get the longest substring
         window_chars.add(s[right])
         current_length = right - left + 1
         max_length = max(max_length, current_length)
 
     return max_length
-
-
-s1 = "abcabcbb"
-print(f"String: '{s1}'")
-print(f"Max Length: {length_of_longest_substring(s1)}") # Output: 3 ("abc")
--- sliding_window_static.py
+++ sliding_window_static.py
@@ -2,23 +2,41 @@
 A generic template for sliding window of fixed size
 """
 def window_fixed_size(nums, k):
-    i = 0
+    left = 0
     result = None
 
-    for j in range(len(nums)):
+    for right in range(len(nums)):
         # Expand the window
         # Add nums[j] to the current window logic
 
         # Ensure window has size of K
-        if (j - i + 1) < k:
+        if (right - left + 1) < k:
             continue
 
         # Update Result
         # Remove nums[i] from window
         # increment i to maintain fixed window size of length k
-        i += 1
+        left += 1
 
     return result
+
+def window_fixed_size2(nums, k):
+    """
+    Could start looping at k if it is fixed
+    """
+    left = 0
+    #base case
+    max_sum = sum(nums[:k])
+    window_sum = max_sum
+
+
+    for right in range(k, len(nums)):
+        window_sum += nums[right] - nums[left]
+        max_sum = max(max_sum, window_sum)
+        left += 1
+
+    return max_sum
+
 
 # find max sum in a contiguous sub array of size k
 def find_max_sum_sub_array(arr, k):
